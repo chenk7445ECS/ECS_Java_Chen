592963647a66d9ff8a5d64b6a7dfcaf8590f97df
chenk7445ECS <billybobjoeman4@gmail.com>
2017-12-01 09:46:35 -0800

    YourInitialsDone

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/43/c04ab34ebfd600171a54916229d4b316
@@ -0,0 +1,13 @@
+public class YourInitials 
+{
+	public static void main( String[] args )
+	{
+		System.out.println("K      K"                   );
+		System.out.println("K    K"                   );
+		System.out.println("K  K"                  );
+		System.out.println("K"                   );
+		System.out.println("K  K"                   );
+		System.out.println("K    K"                   );
+		System.out.println("K      K"                   );
+	}
+}
--- ECS_Java_Unit1/src/YourInitials.java
+++ ECS_Java_Unit1/src/YourInitials.java
@@ -2,12 +2,12 @@ public class YourInitials
 {
 	public static void main( String[] args )
 	{
-		System.out.println("K      K"                   );
-		System.out.println("K    K"                   );
-		System.out.println("K  K"                  );
-		System.out.println("K"                   );
-		System.out.println("K  K"                   );
-		System.out.println("K    K"                   );
-		System.out.println("K      K"                   );
+		System.out.println("K      K                CC");
+		System.out.println("K    K                CC");
+		System.out.println("K  K                CC");
+		System.out.println("K                  C");
+		System.out.println("K  K                CC");
+		System.out.println("K    K                 CC" );
+		System.out.println("K      K                 CC" );
 	}
 }
