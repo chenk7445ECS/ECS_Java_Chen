18b6a68c0e61e319095ffe573ab1c69242135e07
chenk7445ECS <billybobjoeman4@gmail.com>
2017-12-11 10:51:23 -0800

    KeepGuessingCounterdone

--- ECS_Java_Unit6/src/DoubleDice.java
+++ ECS_Java_Unit6/src/DoubleDice.java
@@ -5,21 +5,22 @@ public class DoubleDice
 	public static void main(String[] args )
 	{
 		Random  r = new Random();
-		int randomNum = 1 + r.nextInt(6);
-		int randomNum2 = 1 + r.nextInt(6);
+		int randomNum = 0; 
+		int randomNum2 = 1;
 		
 		System.out.println("HERE COMES THE DICE");
 		
-		 System.out.print("Roll#1:" + randomNum);
-		 System.out.print("Roll#2:" + randomNum2);
-		
-		 
+		  
 		 while ( randomNum != randomNum2)
 		 {
 			 randomNum = 1 + r.nextInt(6);
 			 randomNum2 = 1 + r.nextInt(6);
 			 
-			 System.out.println("The total is" + (randomNum + randomNum2));
+			 System.out.println("Roll#1:" + randomNum);
+			 System.out.println("Roll#2:" + randomNum2);
+			 
+			 
+			 System.out.println("The total is " + (randomNum + randomNum2));
 			 
 			
 			 
