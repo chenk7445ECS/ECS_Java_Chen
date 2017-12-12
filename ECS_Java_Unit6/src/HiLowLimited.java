9206b8cd9db397a720fcda7e04cd541567a4b95d
chenk7445ECS <billybobjoeman4@gmail.com>
2017-12-12 09:55:21 -0800

    HiLowLimiteddone

---
+++ ECS_Java_Unit6/src/HiLowLimited.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HiLowLimited
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		Random r = new Random();
+		
+		int randomNum = 1 + r.nextInt(99);
+		int tries = 0;
+		
+		
+		System.out.println("I'm thinking of a number between 1-100. Try to guess it");
+		int choice = keyboard.nextInt();
+		
+		
+		while ( choice < randomNum )
+		{
+			System.out.println("Sorry, you are too low.");
+			System.out.print("What's your next guess: ");
+			choice = keyboard.nextInt();
+			tries++;
+		}
+		
+		while (choice > randomNum )
+		{
+			System.out.println("Sorry, you are too high.");
+			System.out.println("What's your next guess:");
+			choice = keyboard.nextInt();
+			tries++;
+		}
+		
+
+		if ( choice == randomNum )
+			System.out.println("You guessed it, what are the odds!?");
+		
+		else if ( tries > 7 )
+			System.out.println("Sorry, you didn't guess it in 7 tries. You lose.");
+		keyboard.close();
+				
+			
+		
+	}
+}
