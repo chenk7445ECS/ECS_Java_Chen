e4a6d8b88d18bc5b2ce2738fe80fb00ba5dda987
chenk7445ECS <billybobjoeman4@gmail.com>
2017-12-12 10:41:43 -0800

    NumbGuessCountDone

---
+++ ECS_Java_Unit6DoWhile/src/NumbGuessCount.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumbGuessCount
+{
+	public static void main( String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		Random r = new Random();
+		int randomNum = 1 + r.nextInt(10);
+		int guesses = 0;
+	
+		
+		System.out.println("I have chosen a number between 1 and 10. try to guess it");
+		int guess = keyboard.nextInt();
+		
+		do	
+		{
+			System.out.println("This is incorrect. Guess again");
+			System.out.print("Your guess:");
+			guess = keyboard.nextInt();
+			guesses++;			
+		
+	
+		}
+		while (guess != randomNum);
+	
+		System.out.println("That's right, You're a good guesser.");
+		System.out.println(" It only took you " + guesses + " tries");
+		
+		keyboard.close();
+}
+
+}
