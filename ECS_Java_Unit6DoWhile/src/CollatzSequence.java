66f427039878638e36b853495806f1157de09d39
chenk7445ECS <billybobjoeman4@gmail.com>
2017-12-13 11:11:58 -0800

    CountingMachinedone

--- ECS_Java_Unit6DoWhile/src/CollatzSequence.java
+++ ECS_Java_Unit6DoWhile/src/CollatzSequence.java
@@ -37,6 +37,7 @@ public class CollatzSequence
                     if (counter == 9){
                             counter = 0;
                             System.out.print("\n");
+                     keyboard.close();       
                     }
             }
 
